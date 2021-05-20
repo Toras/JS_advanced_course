fcb1d2a7815caf23313add7c921d50ece640e8bb
Toras <efpan@mail.ru>
2021-05-20 10:12:26 +0700

    added button search, basket

--- script-vue.js
+++ script-vue.js
@@ -19,14 +19,19 @@ const makeGETRequest = (url, callback) => {
 
 const baseUrl = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 const getListUrl = '/catalogData.json';
+const getBasketUrl = '/getBasket.json';
+const addToBasketUrl = '/addToBasket.json';
+const removeFromBasketUrl = '/deleteFromBasket.json';
 
 document.addEventListener('DOMContentLoaded', () => {
     const app = new Vue({
         el: '#app',
         data: {
             goods: [],
+            goodsInCart: [],
             query: '',
             basketName: 'Добавить в корзину',
+            isBasketOpen: false,
         },
         methods: {
             fetchData(callback) {
@@ -36,21 +41,57 @@ document.addEventListener('DOMContentLoaded', () => {
                 });
             },
 
+            fetchCartData(callback) {
+                makeGETRequest(`${baseUrl}${getBasketUrl}`, (data) =>{
+                    data = (JSON.parse(data));
+                    this.goodsInCart = this.transformCartData(data);
+                });
+            },
+
+            transformCartData(list) {
+                return list.contents.map((item) => ({
+                    title: item.product_name,
+                    price: item.price,
+                    quantity: item.quantity,
+                    id: item.id_product
+                }));
+            },
+
             transformData(list) {
                 return list.map((item) => ({
                     title: item.product_name,
                     price: item.price,
-                    id: item.product_name
+                    id: item.id_product
                 }));
-            }
+            },
+
+            basketButton() {
+                this.fetchCartData();
+                this.isBasketOpen = !this.isBasketOpen;
+            },
+
+            searchButton() {
+                this.query = document.querySelector('.header-input').value;
+            },
+
+            clearSearchButton() {
+                document.querySelector('.header-input').value = '';
+                this.query = '';
+            },
         },
         computed: {
             filteredGoods() {
                 return this.goods.filter((product) => (new RegExp(this.query, 'i').test(product.title)))
+            },
+            cartGoods() {
+                const basketPlaceholder = document.querySelector('.basket');
+                basketPlaceholder.style.display = this.isBasketOpen ? 'block': 'none';
+                return this.goodsInCart;
             }
         },
         mounted() {
             this.fetchData();
+            this.fetchCartData();
         }
     })
 })
